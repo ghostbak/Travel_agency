fd53668726a63fbbb6820bf0b084b1c207166236
Aleksander <boss453@gmail.com>
2021-02-13 13:38:22 +0200

    first pull

---
+++ src/test/java/com/sda/projects/Travel_agency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.projects.Travel_agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
