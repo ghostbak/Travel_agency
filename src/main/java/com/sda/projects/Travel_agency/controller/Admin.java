1c89e91620bd31be9d3ede1ed1fd487e4e819110
Aleksander <boss453@gmail.com>
2021-02-21 23:10:14 +0200

    maked mapping in admin controller

--- src/main/java/com/sda/projects/Travel_agency/controller/Admin.java
+++ src/main/java/com/sda/projects/Travel_agency/controller/Admin.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Admin {
@@ -13,10 +15,25 @@ public class Admin {
     @Autowired
     private TripRepository tripRepository;
 
-    @GetMapping("/admin")
+    @GetMapping("/admin_home")
     public String viewAllTrips (Model model) {
     Iterable<Trip> trips = tripRepository.findAll();
     model.addAttribute("trips", trips);
-        return "admin";
+        return "admin_home";
     }
+
+    @GetMapping("/makeTrip")
+    public String makeTrip (Model model) {
+    Trip trip = new Trip();
+    model.addAttribute("trip", trip);
+        return "makeTrip";
+    }
+
+    @PostMapping("/saveTrip")
+    public String saveTrip (@ModelAttribute("trip") Trip trip) {
+        tripRepository.save(trip);
+        return "redirect:/admin_home";
+    }
+
+
 }
